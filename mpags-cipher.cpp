5976b807b591987970e91916c32c7ddedf904a5a
Aleksandra Kulawska <aek851@student.bham.ac.uk>
2020-02-07 15:04:31 +0000

    New exercises

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 int main ()
 {
-//This is a comment
-/* This is a 
-Multiline comment */
-std::cout <<"Hello World!\n";
+double d{4.5};
+int i{4};
+int k{2};
+int c{0};
+c = d/i;
+std::cout << c << std::endl;
+c = i/k;
+std::cout << c << std::endl;
+
+std::string word {"Alex"};
+std::cout << word << std::endl;
+std::cout << word[2] << std::endl;
 }
